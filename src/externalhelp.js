3d9e4bc5fe26676604bfdea8d4b1ca9c561b621b
Nathan Hoel <nathanhoel@gmail.com>
2016-03-28 02:07:36 -0400

    bug fixes

--- src/externalhelp.js
+++ src/externalhelp.js
@@ -17,24 +17,28 @@ var path = require('path');
 var fs = require('fs');
 
 function fromDir(startPath, dirFilter, fileFilter, callback) {
-    var files = fs.readdirSync(startPath);
-    for(var i = 0; i < files.length; i++){
-        var filename = path.join(startPath, files[i]);
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory() && dirFilter.test(filename)) {
-            fromDir(filename, filter, callback);
-        } else if (filter.test(filename)) {
-          callback(filename);
-        }
-    };
+  var files = fs.readdirSync(startPath);
+  for(var i = 0; i < files.length; i++){
+    var filename = path.join(startPath, files[i]);
+    var stat = fs.lstatSync(filename);
+    if (stat.isDirectory() && dirFilter.test(files[i])) {
+      fromDir(filename, dirFilter, fileFilter, callback);
+    } else if (fileFilter.test(filename)) {
+      callback(filename);
+    }
+  };
 };
 
 module.exports = function(robot) {
-  robot.brain.on('loaded', function {
-    fromDir('./node_modules', /^hubot/i, /package\.json/i, function(filename) {
-      var package = require(filename);
-      var scriptPath = path.join(path.dirname(filename), package.main);
-      robot.parseHelp(scriptPath);
+  robot.brain.on('loaded', function() {
+    fromDir('./node_modules', /^hubot-/i, /package\.json/i, function(filename) {
+      try {
+        var package = JSON.parse(fs.readFileSync(filename, 'utf8'));
+        var scriptPath = path.join(path.dirname(filename), package.main);
+        robot.parseHelp(scriptPath);
+      } catch(err) {
+        robot.logger.debug('Could not load help for package: ' + filename)
+      }
     });
   });
 };
